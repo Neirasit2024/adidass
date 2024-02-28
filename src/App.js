a70bf22328fe372c12d6a0dccad349ea7906e824
MacBook Pro <macbookpro@MacBooks-MacBook-Pro.local>
2024-02-28 13:23:16 +0600

    commits

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
+import Header from "./components/Header/header";
 
 
 function App() {
   return (
     <div className="App">
-   
+   <Header/>
     </div>
   );
 }
--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -1,20 +1,40 @@
 import React from 'react';
+import "./header.css"
 import { Link } from 'react-router-dom';
+import { CgAdidas } from "react-icons/cg";
+import { FaSearch } from "react-icons/fa";
+import { RiAdminLine } from "react-icons/ri";
+import { SlLike } from "react-icons/sl";
+import { SlBasketLoaded } from "react-icons/sl";
+
+
 
 const Header = () => {
     return (
         <header id='header'>
             <div className="container">
                 <div className="header">
-                    <Link to="">adbout</Link>
-                    <Link to="">addsa</Link>
-                    <Link to="">dads</Link>
-                    <Link to="">adsda</Link>
-                    <Link to="">dadssds</Link>
+                    <div className="header_logo">
+                        <Link to=""><CgAdidas /></Link>
+                    </div>
+                    <div className="header_pages">
+                        <Link to="">MEN</Link>
+                        <Link to="">WOMEN</Link>
+                        <Link to="">KIDS</Link>
+                        <Link to="">SALE</Link>
+                        <Link to="">3 STRIPE LIFE</Link>
+                    </div>
+                    <div className="header_search_buttons">
+                        <div className="search_button">
+                        <input type="text" placeholder='search product' />
+                        <button><FaSearch /></button>
+                        </div>
+                        <Link to=""><RiAdminLine /></Link>
+                        <Link to=""><SlLike /></Link>
+                        <Link to=""><SlBasketLoaded /></Link>
+                    </div>
                 </div>
             </div>
         </header>
     );
 };
-
-export default Header;
