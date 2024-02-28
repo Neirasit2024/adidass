8591d9c495656659916564615012d6939354865d
MacBook Pro <macbookpro@MacBooks-MacBook-Pro.local>
2024-02-28 11:56:51 +0600

    componentds add

---
+++ src/components/Header/header.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+    return (
+        <header id='header'>
+            <div className="container">
+                <div className="header">
+                    <Link to="">adbout</Link>
+                    <Link to="">addsa</Link>
+                    <Link to="">dads</Link>
+                    <Link to="">adsda</Link>
+                    <Link to="">dadssds</Link>
+                </div>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
